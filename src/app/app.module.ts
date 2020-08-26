7eb0ed7240ce15738b0755bbfb9d0862fd29e8c6
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2020-08-25 19:32:03 -0500

    Home and Login Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomeComponent,
+    LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
